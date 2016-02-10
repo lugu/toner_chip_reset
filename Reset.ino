8634117d0c2fea47f8f107fa62cfd9789a7663d1
Ludovic Guegan <ludovic.guegan@gmail.com>
2016-02-10 21:15:52 +0100

    add commented schema

--- Reset.ino
+++ Reset.ino
@@ -1,44 +1,49 @@
 
 #include <Wire.h>
-#define eeprom 0x50
+#define eeprom 0x50 // 0x50 = 80 = 1010000
 
 void writeEEPROM(int device, unsigned int address, byte data ) {
-  Wire.beginTransmission(device);
-  Wire.write((int)(address >> 8));   // MSB
-  Wire.write((int)(address & 0xFF)); // LSB
-  Wire.write(data);
-  Wire.endTransmission();
+	Wire.beginTransmission(device);
+	Wire.write((int)(address >> 8));   // MSB
+	Wire.write((int)(address & 0xFF)); // LSB
+	Wire.write(data);
+	Wire.endTransmission();
 }
 
 byte readEEPROM(int device, unsigned int address) {
-  byte rdata = 0xFF;
-  Wire.beginTransmission(device);
-  Wire.write((int)(address >> 8));   // MSB
-  Wire.write((int)(address & 0xFF)); // LSB
-  Wire.endTransmission();
-  Wire.requestFrom(device,1);
-  if (Wire.available()) {
-	  rdata = Wire.read();
-  }
-  return rdata;
+	byte rdata = 0xFF;
+	Wire.beginTransmission(device);
+	Wire.write((int)(address >> 8));   // MSB
+	Wire.write((int)(address & 0xFF)); // LSB
+	Wire.endTransmission();
+	Wire.requestFrom(device,1);
+	if (Wire.available()) {
+		rdata = Wire.read();
+	}
+	return rdata;
 }
 
 void setup(void){
-  Wire.begin();
+	Wire.begin();
 
-  Serial.begin(9600);
-  while (!Serial.available()) {
-	  ; // wait for serial port to connect
-  }
-  Serial.println("Let's start!");
+	Serial.begin(115200);
+	while (!Serial.available()) {
+		; // wait for serial port to connect
+	}
+	Serial.println("Let's start!");
 
-  unsigned int address = 0;
-  Serial.println("Reading first bytes of the EEPROM");
+	unsigned int address = 0;
+	unsigned int config = 0;
 
-  for(address = 0; address<10; address++) {
-     Serial.print(readEEPROM(eeprom, address), HEX); 
-     Serial.print(", ");
-  }
+	for(config = 0; config<8; config++) {
+		Serial.print("DUMP EEPROM ");
+		Serial.println(eeprom + config);
+		for(address = 0; address<1024; address++) {
+			Serial.print(readEEPROM(eeprom + config, address), HEX); 
+			Serial.print(", ");
+		}
+		Serial.println("");
+	}
 }
 
 void loop(){
